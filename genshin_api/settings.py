3dbd6279127fd385b904831de6669fb5ebbca2b4
jihoonchoi724 <snesiscool@gmail.com>
2023-09-09 22:10:28 -0700

    Further changes for Vercel

--- genshin_api/settings.py
+++ genshin_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--wt1jkc!f!74_(cmxf-^=3ke0%uptr1u5z3lgz)$w-$re3+qf@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
